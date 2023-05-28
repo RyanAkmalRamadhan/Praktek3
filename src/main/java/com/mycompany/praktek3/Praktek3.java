83a57d8e4ccdd234d48b91de7fc05c232b722ff8
RyanAkmalRamadhan <134803527+RyanAkmalRamadhan@users.noreply.github.com>
2023-05-28 22:02:14 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/praktek3/Praktek3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praktek3;
+
+/**
+ *
+ * @author User
+ */
+public class Praktek3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
