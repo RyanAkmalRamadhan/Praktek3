9d6f723f84657941db81f682c62b18552a676038
RyanAkmalRamadhan <134803527+RyanAkmalRamadhan@users.noreply.github.com>
2023-05-28 22:33:20 +0800

    attribute dan method

--- src/main/java/com/mycompany/praktek3/SepedaMotor.java
+++ src/main/java/com/mycompany/praktek3/SepedaMotor.java
@@ -1,5 +1,9 @@
 package com.mycompany.praktek3;
 
 public class SepedaMotor extends Sepeda{
+    private String jenisMotor;
     
+    void throttle(){
+        System.out.println("MBEEERRRRR......");
+    }
 }
