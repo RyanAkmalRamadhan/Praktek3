9c5c3cc503a5f1024cd4d13370d21f20bca78bdd
RyanAkmalRamadhan <134803527+RyanAkmalRamadhan@users.noreply.github.com>
2023-05-28 22:05:20 +0800

    Membuat atribut

--- src/main/java/com/mycompany/praktek3/Sepeda.java
+++ src/main/java/com/mycompany/praktek3/Sepeda.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.praktek3;
 
-/**
- *
- * @author User
- */
 public class Sepeda {
-    
+  String merk;
+  String warna;
+  double harga;
+         
 }
