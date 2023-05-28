b96ccdb4d1fa7784bdd2f873dfae4c4eb7b085ae
RyanAkmalRamadhan <134803527+RyanAkmalRamadhan@users.noreply.github.com>
2023-05-28 22:19:11 +0800

    set atribut private

--- src/main/java/com/mycompany/praktek3/Sepeda.java
+++ src/main/java/com/mycompany/praktek3/Sepeda.java
@@ -1,9 +1,9 @@
 package com.mycompany.praktek3;
 
 public class Sepeda {
-  String merk;
-  String warna;
-  double harga;
+  private String merk;
+  private String warna;
+  private double harga;
   
   void cetakInfo(){
       System.out.println("Merk \t: "+merk+"\n"+
